d6b59a9d91955bcd86c0dd251b9b26cad29642e9
yui1412 <aaayui1412@mailbox.unideb.hu>
2025-03-07 16:30:54 +0100

    build: delete the now unused commons-lang3 dependency from pom.xml

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,10 +1,10 @@
 package nationalize;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Data;
 import java.util.List;
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 
+@Data
 public class Nationality {
 
     private long count;
@@ -13,70 +13,9 @@ public class Nationality {
     @SerializedName("country")
     private List<Country> countries;
 
-    public Nationality(long count, String name, List<Country> countries) {
-        this.count = count;
-        this.name = name;
-        this.countries = countries;
-    }
-
-    public long getCount() {
-        return count;
-    }
-
-    public void setCount(long count) {
-        this.count = count;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Country> getCountries() {
-        return countries;
-    }
-
-    public void setCountries(List<Country> countries) {
-        this.countries = countries;
-    }
-
-    @Override
-    public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-    }
-
+    @Data
     public static class Country {
-
         private String countryId;
         private float probability;
-
-        public Country(String countryId, float probability) {
-            this.countryId = countryId;
-            this.probability = probability;
-        }
-
-        public String getCountryId() {
-            return countryId;
-        }
-
-        public void setCountryId(String countryId) {
-            this.countryId = countryId;
-        }
-
-        public float getProbability() {
-            return probability;
-        }
-
-        public void setProbability(float probability) {
-            this.probability = probability;
-        }
-
-        @Override
-        public String toString() {
-            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-        }
     }
 }
